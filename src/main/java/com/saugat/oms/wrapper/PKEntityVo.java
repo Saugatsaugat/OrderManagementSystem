baff2519bdc1ec8b18c87253f057e0f0d1b293f4
Saugat Thapa <saugatthapa2073@gmail.com>
2026-05-25 08:54:51 -0500

    Create Primary Key entity wrapper class

---
+++ src/main/java/com/saugat/oms/wrapper/PKEntityVo.java
@@ -0,0 +1,18 @@
+package com.saugat.oms.wrapper;
+
+import java.io.Serializable;
+
+public class PKEntityVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
