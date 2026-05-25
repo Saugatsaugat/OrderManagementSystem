4e6a46e50c3aea24038d4470ac4917ec33c7430e
Saugat Thapa <saugatthapa2073@gmail.com>
2026-05-25 08:55:26 -0500

    Create Entity Change Tracker Wrapper class for DTO

---
+++ src/main/java/com/saugat/oms/wrapper/EntityChangeTrackerVo.java
@@ -0,0 +1,43 @@
+package com.saugat.oms.wrapper;
+
+import java.time.LocalDateTime;
+
+public class EntityChangeTrackerVo extends PKEntityVo {
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
