a826e6331cec1db110d1b48bc71aed27e61f2a9d
Saugat Thapa <53994209+Saugatsaugat@users.noreply.github.com>
2026-05-31 19:11:27 -0500

    Create User Wrapper

---
+++ src/main/java/com/saugat/oms/wrapper/UserVo.java
@@ -0,0 +1,48 @@
+package com.saugat.oms.wrapper;
+
+public class UserVo extends EntityChangeTrackerVo{
+
+    private String email;
+    private String mobile;
+    private String pass;
+
+    public UserVo() {
+    }
+
+    public UserVo(String email, String mobile, String pass) {
+        this.email = email;
+        this.mobile = mobile;
+        this.pass = pass;
+    }
+
+    public UserVo(Long id, String email, String mobile, String pass) {
+        super.setId(id);
+        this.email = email;
+        this.mobile = mobile;
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
