71251083d0554501f53e32fd360561961ccbb77a
Saugat Thapa <53994209+Saugatsaugat@users.noreply.github.com>
2026-06-02 15:55:13 -0500

    Create User Controller

---
+++ src/main/java/com/saugat/oms/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.saugat.oms.controller;
+
+import com.saugat.oms.service.IService;
+import com.saugat.oms.wrapper.UserVo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController extends AbstractController<UserVo, Long> {
+
+    private final IService<UserVo, Long> service;
+    public UserController(IService<UserVo, Long> service){
+        this.service = service;
+    }
+
+    @Override
+    protected IService<UserVo, Long> getService() {
+        return this.service;
+    }
+
+    @Override
+    public String getResourceName() {
+        return "User";
+    }
+}
