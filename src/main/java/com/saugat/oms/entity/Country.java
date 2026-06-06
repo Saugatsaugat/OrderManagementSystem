4a9bb3c01b4b8d0ce10b128dec2994abb7991f89
Saugat Thapa <53994209+Saugatsaugat@users.noreply.github.com>
2026-06-06 00:45:30 -0500

    Create Country entity, wrapper, mapper, repository, service, and controller

---
+++ src/main/java/com/saugat/oms/entity/Country.java
@@ -0,0 +1,45 @@
+package com.saugat.oms.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country extends EntityChangeTracker{
+    @Column(name = "code", unique = true, nullable = false)
+    private String code;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Country(Long id, String code, String name) {
+        this.setId(id);
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
