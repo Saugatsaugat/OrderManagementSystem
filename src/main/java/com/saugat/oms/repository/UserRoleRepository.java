f560e358aa2ec17e175601d6273767ac9283ee6e
Saugat Thapa <53994209+Saugatsaugat@users.noreply.github.com>
2026-06-07 21:43:28 -0500

    Create UserRole Repository class

---
+++ src/main/java/com/saugat/oms/repository/UserRoleRepository.java
@@ -0,0 +1,8 @@
+package com.saugat.oms.repository;
+
+import com.saugat.oms.entity.UserRole;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends IBaseRepository<UserRole, Long>{
+}
